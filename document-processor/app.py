3fff5fe91ed5e1eee17c813af90b6b576663cd31
Philbertano <92738246+Philbertano@users.noreply.github.com>
2024-05-20 10:15:01 +0200

    Update app.py

--- document-processor/app.py
+++ document-processor/app.py
@@ -1,7 +1,7 @@
 import os
 import boto3
 import tempfile
-#from langchain.embeddings.bedrock import BedrockEmbeddings
+from langchain.embeddings.bedrock import BedrockEmbeddings
 #from langchain.text_splitter import CharacterTextSplitter
 #from langchain.document_loaders import PDFLoader
 #from vectordb import connect, LanceDB
