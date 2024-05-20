431dffef49d1a2a0a2291d8195c76eea81f8b7fc
EC2 Default User <ec2-user@ip-172-31-83-222.ec2.internal>
2024-05-20 09:31:07 +0000

    App.py

--- document-processor/app.py
+++ document-processor/app.py
@@ -1,11 +1,30 @@
 import os
 import boto3
 import tempfile
-from langchain.embeddings.bedrock import BedrockEmbeddings
-from langchain.text_splitter import CharacterTextSplitter
-from langchain.document_loaders import PDFLoader
-from vectordb import connect, LanceDB
+from langchain_community.embeddings.bedrock import BedrockEmbeddings
+from langchain_community.document_loaders import PyPDFLoader
+from langchain_text_splitters import CharacterTextSplitter
+from langchain_community.vectorstores import LanceDB
 from botocore.exceptions import ClientError
+
+
+# Env vars
+lance_db_src = os.environ['s3BucketName']
+lance_db_table = os.environ['lanceDbTable']
+aws_region = os.environ['region']
+
+text_splitter = CharacterTextSplitter(
+    separator="\n\n",
+    chunk_size=1000,
+    chunk_overlap=200,
+    length_function=len,
+    is_separator_regex=False,
+)
+
+embeddings = BedrockEmbeddings(region=aws_region, model='amazon.titan-embed-text-v1')
+
+s3_client = boto3.client('s3', region_name=aws_region)
+
 
 def handler(event, context):
     print("Hello world")
