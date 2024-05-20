9e79875311c94299c22a3b1d7488627f9d4c7943
EC2 Default User <ec2-user@ip-172-31-83-222.ec2.internal>
2024-05-20 13:16:49 +0000

    Test

--- document-processor/app.py
+++ document-processor/app.py
@@ -56,7 +56,7 @@ def lambda_handler(event, context):
     
     asyncio.run(create_directory())
 
-    download_object(bucket_name, object_key, file_path)
+    #download_object(bucket_name, object_key, file_path)
 
     print("Hello world")
     
