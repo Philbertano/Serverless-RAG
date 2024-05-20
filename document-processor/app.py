b452d0045da06a3355a4818803e915ae2fe05479
Philbertano <92738246+Philbertano@users.noreply.github.com>
2024-05-20 10:17:56 +0200

    Update app.py

--- document-processor/app.py
+++ document-processor/app.py
@@ -2,7 +2,7 @@ import os
 import boto3
 import tempfile
 from langchain.embeddings.bedrock import BedrockEmbeddings
-#from langchain.text_splitter import CharacterTextSplitter
+from langchain.text_splitter import CharacterTextSplitter
 #from langchain.document_loaders import PDFLoader
 #from vectordb import connect, LanceDB
 from botocore.exceptions import ClientError
