fd286eefd83f7fc071f7a1a28e88e24c504c42a3
EC2 Default User <ec2-user@ip-172-31-83-222.ec2.internal>
2024-06-09 13:28:02 +0000

    QueryFunction

---
+++ document-processor/document-processor.py
@@ -0,0 +1,84 @@
+import os
+import boto3
+import tempfile
+import json
+from langchain_community.embeddings.bedrock import BedrockEmbeddings
+from langchain_community.document_loaders import PyPDFLoader
+from langchain_text_splitters import CharacterTextSplitter
+from langchain_community.vectorstores import LanceDB
+from botocore.exceptions import ClientError
+
+
+# Env vars
+lance_db_src = os.environ['s3BucketName']
+lance_db_table = os.environ['lanceDbTable']
+aws_region = os.environ['region']
+
+text_splitter = CharacterTextSplitter(
+    separator="\n\n",
+    chunk_size=1000,
+    chunk_overlap=200,
+    length_function=len,
+    is_separator_regex=False,
+)
+
+s3_client = boto3.client('s3', region_name=aws_region)
+
+bedrock_client = boto3.client(service_name='bedrock-runtime', region_name=aws_region)
+
+model_id = "amazon.titan-embed-text-v1"
+
+embeddings = BedrockEmbeddings(
+    client=bedrock_client,
+    model_id=model_id
+    )
+
+def download_object(bucket_name, object_key, download_path):
+    try:
+        # Get the object from the Amazon S3 bucket
+        response = s3_client.get_object(Bucket=bucket_name, Key=object_key)
+        # Stream the object to a file
+        with open(download_path, 'wb') as f:
+            f.write(response['Body'].read())
+        print(f'File downloaded to {download_path}')
+    except ClientError as e:
+        print('Error:', e)
+
+
+def create_directory():
+    tmp_path = os.path.join('/tmp', 'documents')
+    try:
+        os.makedirs(tmp_path, exist_ok=True)
+        print(f'Directory created at: {tmp_path}')
+    except OSError as e:
+        print('Error creating directory:', e)
+
+
+def lambda_handler(event, context):
+    # The S3 event contains details about the uploaded object
+    bucket_name = event['Records'][0]['s3']['bucket']['name']
+    object_key = event['Records'][0]['s3']['object']['key'].replace('+', ' ')
+    file_path = f'/tmp/{object_key}'
+
+    create_directory()
+
+    download_object(bucket_name, object_key, file_path)
+
+    try:
+        loader = PyPDFLoader(file_path)
+        docs = loader.load_and_split(text_splitter)
+    except Exception as e:
+        print('Error loading documents:', e)
+        return {'statusCode': 500, 'body': json.dumps({'message': str(e)})}
+
+    dir = f's3://{lance_db_src}/embeddings'
+    
+    try:
+        db = LanceDB(uri=dir, region=aws_region, embedding=embeddings, table_name=lance_db_table)
+    except Exception as e:
+        print('Error connecting to LanceDB:', e)
+        return {'statusCode': 500, 'body': json.dumps({'message': str(e)})}
+
+    LanceDB.from_documents(docs, embeddings)
+
+    return {'statusCode': 201, 'body': json.dumps({'message': 'OK'})}
---
+++ query-processor/query-processor.py
@@ -0,0 +1,61 @@
+import os
+import boto3
+import tempfile
+import json
+from langchain_community.embeddings.bedrock import BedrockEmbeddings
+from langchain_community.document_loaders import PyPDFLoader
+from langchain_text_splitters import CharacterTextSplitter
+from langchain_community.vectorstores import LanceDB
+from botocore.exceptions import ClientError
+
+
+# Env vars
+lance_db_src = os.environ['s3BucketName']
+lance_db_table = os.environ['lanceDbTable']
+aws_region = os.environ['region']
+
+# Clients
+s3_client = boto3.client('s3', region_name=aws_region)
+
+bedrock_client = boto3.client(service_name='bedrock-runtime', region_name=aws_region)
+
+def format_docs(docs):
+    return "\n\n".join(doc.page_content for doc in docs)
+
+def lambda_handler(event, context):
+    
+    try:
+        vector_store = LanceDB(
+            uri=lance_db_src,
+            region=aws_region,
+            table_name=lance_db_table
+            )
+    except Exception as e:
+        print('Error connecting to LanceDB:', e)
+        return {'statusCode': 500, 'body': json.dumps({'message': str(e)})}
+
+    retriever = vector_store.as_retriever()
+    
+    # Create prompt
+    prompt = hub.pull("rlm/rag-prompt")
+
+    # Initialize the Bedrock LLM
+    bedrock_llm = BedrockLLM(
+        model_id="anthropic.claude-v2:1"
+    )
+
+    # Create RAG chain
+    rag_chain = (
+        {"context": retriever | format_docs, "question": RunnablePassthrough()}
+        | prompt
+        | bedrock_llm
+        | StrOutputParser()
+    )
+
+    # Query the RAG chain
+    response = rag_chain.invoke("List all the bedrock models available?")
+
+    # Print response
+    print(response)
+
+    return {'statusCode': 201, 'body': json.dumps({'message': 'OK'})}
